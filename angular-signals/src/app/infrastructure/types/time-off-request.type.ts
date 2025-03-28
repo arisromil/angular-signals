c422c4fc6698d910357e2b7b6e4958de3474ebec
arisromil <arisromil@yahoo.com>
2025-03-28 11:27:25 -0600

    Create time-off-request.type.ts

---
+++ angular-signals/src/app/infrastructure/types/time-off-request.type.ts
@@ -0,0 +1,10 @@
+export type TimeOffRequest = {
+    id: number;
+    employeeId: number;
+    startDate: string;
+    endDate: string;
+    type: 'Vacation' | 'Sick Leave' | 'Maternity Leave' |
+    'Paternity Leave' | 'Other';
+    status: 'Pending' | 'Approved' | 'Rejected';
+    comment?: string;
+};
